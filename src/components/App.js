b0c467db02f687fbe7662ea4ec032d701c7f8476
ImadEM21 <imad.elmahrad98@gmail.com>
2021-02-06 21:07:30 +0100

    change favicon and icons for phones, change meta description and keywords

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
