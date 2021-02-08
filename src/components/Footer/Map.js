ad823d43e4214acd88bbf0dfa279d3442de4a9ab
ImadEM21 <imad.elmahrad98@gmail.com>
2021-02-08 14:21:43 +0100

    fix the google maps marker

--- src/components/Footer/Map.js
+++ src/components/Footer/Map.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
-import { useGoogleMaps } from 'react-hook-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 
-const Map = () => {
-    const { ref, map, google } = useGoogleMaps(
-        "AIzaSyAJ18unufJihyeiWr5-RsXURTUACQOTw9s", 
-        {
-            center: {lat: 45.1930081, lng:5.7302905},
-            zoom: 14
-        }
-    );
+class Map extends React.Component  {
 
-    return (
-        <Col className="d-flex justify-content-center">
-            <div ref={ref} style={{width: '70%', height: '100%'}} />
-        </Col>
-    )
-}
+    render () {
+        const GoogleMapExample = withGoogleMap(props => (
+            <GoogleMap 
+                defaultCenter={{lat: 45.1930081, lng: 5.7302905}}
+                defaultZoom={15}
+            >
+                {props.isMarkerShown && 
+                    <Marker position={{lat: 45.1930081, lng: 5.7302905}} />
+                }
+            </GoogleMap>
+        ))
+    
+        return (
+            <Col className="d-flex justify-content-center">
+                <GoogleMapExample
+                    isMarkerShown
+                    containerElement={<div style={{height: '100%', width: '70%'}} />}
+                    mapElement={<div style={{height: '100%', width: '100%'}} />}
+                />
+            </Col>
+        );
+    };
+    
+};
 
  
 export default Map;
