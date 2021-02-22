92c42833510fbb00193b9028dbb2fe1d65bcc216
ImadEM21 <imad.elmahrad98@gmail.com>
2021-02-22 16:20:16 +0100

    confirm order by admin

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from "react";
-import uniqid from 'uniqid';
-import { Container, Row, Col, Table, Button, Form, ListGroup } from "react-bootstrap";
+import uniqid from "uniqid";
+import {
+  Container,
+  Row,
+  Col,
+  Table,
+  Button,
+  Form,
+  ListGroup,
+  Alert
+} from "react-bootstrap";
 import firebase from "../../firebase";
 
 const Admin = () => {
   const [isValidated, setIsValidated] = useState(false);
+  const [validateSuccess, setValidateSuccess] = useState(false);
+  const [validateError, setValidateError] = useState(false);
+
   const [ordersListed, setOrdersListed] = useState();
+  const [timeCustom, setTimeCustom] = useState("");
+  const [timeSelected, setTimeSelected] = useState("20 minutes");
+
 
   const totalPrice = (array) => {
     let total = 0;
@@ -20,15 +35,46 @@ const Admin = () => {
     return s.charAt(0).toUpperCase() + s.slice(1);
   };
 
+  const confirmOrder = order => e => {
+    e.preventDefault();
+    let time = "";
+
+    if (timeCustom.length > 0) {
+      time = timeCustom;
+    } else {
+      time = timeSelected;
+    }
+
+    const orderRef = firebase.database().ref('orders').child(order.id);
+    orderRef.update({
+      confirmed: true,
+      timeEstimation: time
+    })
+    .then(() => {
+      setValidateError(false);
+      setValidateSuccess(true);
+    })
+    .catch(err => {
+      console.error('Error updating order', err);
+      setValidateSuccess(false);
+      setValidateError(true);
+    });
+  };
+
+  const refuseOrder = order => {
+    const orderRef = firebase.database().ref('orders').child(order.id);
+    orderRef.remove();
+  };
+
   useEffect(() => {
     const orderRef = firebase.database().ref("orders");
     orderRef.on("value", (snapshot) => {
-    const orders = snapshot.val();
-    const ordersList = [];
-    for (let id in orders) {
+      const orders = snapshot.val();
+      const ordersList = [];
+      for (let id in orders) {
         ordersList.push({ id, ...orders[id] });
-    }
-    setOrdersListed(ordersList);
+      }
+      setOrdersListed(ordersList);
     });
   }, []);
 
@@ -70,46 +116,80 @@ const Admin = () => {
                         : "Utilisateur invité"}
                     </td>
                     <td>
-                    {order.order.map((recapItem) =>
-                      recapItem.map((item) => (
-                        <ListGroup horizontal="md" key={uniqid()}>
-                          <ListGroup.Item className="list-group-admin"><strong>{item.name}</strong></ListGroup.Item>
-                          <ListGroup.Item className="list-group-admin"><strong>Menu:</strong> {item.menu ? "Oui" : "Non"}</ListGroup.Item>
-                          <ListGroup.Item className="list-group-admin"><strong>Suppléments:</strong> {item.supplements ? item.supplements : "Aucun"}</ListGroup.Item>
-                          <ListGroup.Item className="list-group-admin"><strong>Boissons:</strong> {capitalize(item.boisson)}</ListGroup.Item>
-                          <ListGroup.Item className="list-group-admin"><strong>Prix:</strong> {item.prix}€</ListGroup.Item>
-                        </ListGroup>
-                      ))
-                    )}
+                      {order.order.map((recapItem) =>
+                        recapItem.map((item) => (
+                          <ListGroup horizontal="md" key={uniqid()}>
+                            <ListGroup.Item className="list-group-admin">
+                              <strong>{item.name}</strong>
+                            </ListGroup.Item>
+                            <ListGroup.Item className="list-group-admin">
+                              <strong>Menu:</strong> {item.menu ? "Oui" : "Non"}
+                            </ListGroup.Item>
+                            <ListGroup.Item className="list-group-admin">
+                              <strong>Suppléments:</strong>{" "}
+                              {item.supplements ? item.supplements : "Aucun"}
+                            </ListGroup.Item>
+                            <ListGroup.Item className="list-group-admin">
+                              <strong>Boissons:</strong>{" "}
+                              {capitalize(item.boisson)}
+                            </ListGroup.Item>
+                            <ListGroup.Item className="list-group-admin">
+                              <strong>Prix:</strong> {item.prix}€
+                            </ListGroup.Item>
+                          </ListGroup>
+                        ))
+                      )}
                     </td>
                     <td>Prix total: {totalPrice(order.order)}</td>
                     <td>
                       {isValidated ? (
-                        <Form>
+                        <>
+                        <Form onSubmit={confirmOrder(order)}>
                           <Form.Group controlId="timeEstimation">
                             <Form.Label>Temps estimé de préparation</Form.Label>
-                            <Form.Control as="select" defaultValue="20 minutes">
-                              <option>10 minutes</option>
-                              <option>20 minutes</option>
-                              <option>30 minutes</option>
-                              <option>40 minutes</option>
-                              <option>50 minutes</option>
+                            <Form.Control
+                              as="select"
+                              value={timeSelected}
+                              onChange={(e) => {
+                                setTimeSelected(e.target.value);
+                                console.log(e.target.value)
+                              }}
+                            >
+                              <option value="10 minutes">10 minutes</option>
+                              <option value="20 minutes">20 minutes</option>
+                              <option value="30 minutes">30 minutes</option>
+                              <option value="40 minutes">40 minutes</option>
+                              <option value="50 minutes">50 minutes</option>
                             </Form.Control>
                           </Form.Group>
                           <Form.Group controlId="customizedTime">
                             <Form.Label>Un autre temps d'estimation</Form.Label>
-                            <Form.Control type="text" placeholder="1 heure" />
+                            <Form.Control
+                              type="text"
+                              placeholder="1 heure"
+                              value={timeCustom}
+                              onChange={(e) => setTimeCustom(e.target.value)}
+                            />
                           </Form.Group>
-                          <Button variant="success" className="mr-3">
+                          <Button
+                            variant="success"
+                            className="mr-3 my-2"
+                            type="submit"
+                          >
                             Valider la commande
                           </Button>
                           <Button
                             variant="secondary"
+                            type="reset"
+                            className="my-2"
                             onClick={() => setIsValidated(false)}
                           >
                             Annuler
                           </Button>
                         </Form>
+                        {validateSuccess && <Alert variant="success" className="my-3">Commande validée</Alert>}
+                        {validateError && <Alert variant="danger" className="my-3">Un problème est survenu</Alert>}
+                        </>
                       ) : (
                         <>
                           <Button
@@ -119,7 +199,7 @@ const Admin = () => {
                           >
                             Valider
                           </Button>
-                          <Button variant="danger">Refuser</Button>
+                          <Button variant="danger" type="button" onClick={() => refuseOrder(order)}>Refuser</Button>
                         </>
                       )}
                     </td>
