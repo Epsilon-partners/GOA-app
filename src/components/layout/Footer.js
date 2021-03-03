40f469f8eb4f4f3d54a342718a76816b90879126
ImadEM21 <imad.elmahrad98@gmail.com>
2021-03-03 10:03:07 +0100

    fix style footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -11,22 +11,21 @@ import {
 import { faEnvelope, faClock } from "@fortawesome/free-regular-svg-icons";
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
 import Maps from "./Maps";
-import tripadvisor from '../../img/tripadvisor.svg';
-import instagram from '../../img/instagram.svg';
-import background from '../../img/mandala.png';
-
+import tripadvisor from "../../img/tripadvisor.svg";
+import instagram from "../../img/instagram.svg";
+import background from "../../img/mandala.png";
 
 const Footer = () => {
   return (
-    <Container fluid as="footer" id="footer" className="py-5">
+    <Container fluid as="footer" id="footer" className="py-5 mt-5">
       <Row>
-        <Col md={2}>
-            <img src={background} alt="Mandala" className="mandala-footer" />
+        <Col md={2} className="d-none d-md-block">
+          <img src={background} alt="Mandala" className="mandala-footer" />
         </Col>
         <Col md={6}>
           <h5 className="goa-footer-title mb-3">Goa restaurant</h5>
           <Row>
-            <Col md={6} className="mb-4">
+            <Col md={7} className="mb-4">
               <FontAwesomeIcon
                 icon={faPhoneAlt}
                 color="black"
@@ -52,58 +51,107 @@ const Footer = () => {
               />{" "}
               14 rue Brocherie, 38000 Grenoble
             </Col>
-            <Col md={6}>
-              <FontAwesomeIcon icon={faClock} className="mr-3 info-footer-icons" size="lg" />
-              Tous les jours de: <br />
-              11h30 - 15H00
-              <br />
-              18h00 - 01h00
+            <Col md={5} className="d-flex">
+              <div>
+                <FontAwesomeIcon
+                  icon={faClock}
+                  className="mr-3 info-footer-icons"
+                  size="lg"
+                />
+              </div>
+              <div>
+                Tous les jours de: <br />
+                11h30 - 15H00
+                <br />
+                18h00 - 01h00
+              </div>
             </Col>
-            <Col md={6}>
+            <Col md={7}>
               <Maps />
             </Col>
-            <Col md={6}>
-              <h6 className="text-dark">Equipements</h6>
+            <Col md={5}>
+              <h5 className="goa-footer-title mb-3">Equipements</h5>
               <div>
-                <FontAwesomeIcon icon={faWifi} color="black" className="mr-3 info-footer-icons" size="lg" />{" "}
-                Wifi <br />
-                <FontAwesomeIcon
-                  icon={faTv}
-                  color="black"
-                  className="mr-3 info-footer-icons"
-                  size="lg"
-                />{" "}
-                Télévision <br />
-                <FontAwesomeIcon
-                  icon={faWind}
-                  color="black"
-                  className="mr-3 info-footer-icons"
-                  size="lg"
-                />{" "}
-                Air conditionné
+                <div className="d-flex justify-content-start">
+                  <div className="footer-wrapper-icon">
+                    <FontAwesomeIcon
+                      icon={faWifi}
+                      color="black"
+                      className="mr-3 info-footer-icons"
+                      size="lg"
+                    />
+                  </div>
+                  <div className="ml-2">Wifi</div>
+                </div>
+                <div className="d-flex justify-content-start">
+                  <div className="footer-wrapper-icon">
+                    <FontAwesomeIcon
+                      icon={faTv}
+                      color="black"
+                      className="mr-3 info-footer-icons"
+                      size="lg"
+                    />
+                  </div>
+                  <div className="ml-2">Télévision</div>
+                </div>
+                <div className="d-flex justify-content-start">
+                  <div className="footer-wrapper-icon">
+                    <FontAwesomeIcon
+                      icon={faWind}
+                      color="black"
+                      className="mr-3 info-footer-icons"
+                      size="lg"
+                    />
+                  </div>
+                  <div className="ml-2">Air conditionné</div>
+                </div>
               </div>
             </Col>
           </Row>
         </Col>
-        <Col md={4} className="d-flex flex-column border-left border-dark">
-          <h5 className="goa-footer-title mb-3">Suivez nous</h5>
+        <Col md={4} className="d-flex flex-column border-left border-dark pl-4 mt-4 mt-md-0">
+          <h5 className="goa-footer-title mb-4">Suivez-nous</h5>
           <div className="d-flex flex-row justify-content-start">
             <div className="footer-border-icons brand-fb mr-3">
-                <FontAwesomeIcon icon={faFacebookF} style={{height: '48px', width: '48px'}} className="mx-1" />
+              <FontAwesomeIcon
+                icon={faFacebookF}
+                style={{ height: "48px", width: "48px" }}
+                className="mx-1"
+              />
             </div>
             <div className="footer-border-icons brand-ig mr-3">
-                <img src={instagram} alt="Instagram Logo" height="48px" width="48px" className="mx-1" />
+              <img
+                src={instagram}
+                alt="Instagram Logo"
+                height="48px"
+                width="48px"
+                className="mx-1"
+              />
             </div>
             <div className="footer-border-icons brand-ta">
-                <img src={tripadvisor} alt="TripAdvisor Logo" height="48px" width="48px" className="mx-1" />
+              <img
+                src={tripadvisor}
+                alt="TripAdvisor Logo"
+                height="48px"
+                width="48px"
+                className="mx-1"
+              />
             </div>
           </div>
           <ListGroup className="border-0 my-5">
-            <ListGroup.Item className="footer-list-copyright p-0"><a href="#mentions-légales">Mentions légales</a></ListGroup.Item>
-            <ListGroup.Item className="footer-list-copyright p-0"><a href="#donnees-personnelles">Données personnelles</a></ListGroup.Item>
-            <ListGroup.Item className="footer-list-copyright p-0"><a href="#conditions-generales">Conditions générales de vente</a></ListGroup.Item>
+            <ListGroup.Item className="footer-list-copyright p-0">
+              <a href="#mentions-légales">Mentions légales</a>
+            </ListGroup.Item>
+            <ListGroup.Item className="footer-list-copyright p-0">
+              <a href="#donnees-personnelles">Données personnelles</a>
+            </ListGroup.Item>
+            <ListGroup.Item className="footer-list-copyright p-0">
+              <a href="#conditions-generales">Conditions générales de vente</a>
+            </ListGroup.Item>
           </ListGroup>
-          <p className="text-dark mt-1">&copy; Copyright 2021 GOA Indian Fast-Food. All Rights Reserved.</p>
+          <p className="text-dark mt-1">
+            &copy; Copyright 2021 GOA Indian Fast-Food. All Rights Reserved.
+          </p>
         </Col>
       </Row>
     </Container>
